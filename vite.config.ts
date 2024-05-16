097a022bf068c1b1ae2e91bcc9ff71efa9d57880
Brendan Dash <me@aiwan.run>
2024-05-16 20:51:08 +0800

    feat: react-compiler

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", {}]],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "~": fileURLToPath(new URL("./src", import.meta.url)),
